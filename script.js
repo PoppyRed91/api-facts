fe1015fb98b3d4ae870c671c835d92540d281aad
PoppyRed91 <pfercek@gmail.com>
2023-11-16 20:09:33 +0100

    HTML, CSS and JS done

---
+++ script.js
@@ -0,0 +1,42 @@
+"use strict"
+// cats
+
+async function getCatFact() {
+    try {
+        const response = await fetch("http://192.168.50.69:8765/cats");
+        const catFacts = await response.json();
+        let catFactEl = document.querySelector(".cat-description");
+        catFactEl.innerText = catFacts["fact"];
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+getCatFact()
+
+let catButton = document.querySelector(".cat-button");
+catButton.addEventListener("click", function () {
+    getCatFact();
+});
+
+// dogs
+
+async function getDogFact() {
+    try {
+        const response = await fetch("http://192.168.50.69:8765/dogs");
+        const dogFacts = await response.json();
+        let dogFactEl = document.querySelector(".dog-description");
+        dogFactEl.innerText = dogFacts["fact"];
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+getDogFact()
+
+let dogButton = document.querySelector(".dog-button");
+dogButton.addEventListener("click", function () {
+    getDogFact();
+});
